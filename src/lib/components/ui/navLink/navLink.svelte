2c5a631c82b3de671da6d920779ae202458c4886
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2024-12-10 10:14:58 +0100

    feat: create sidebar part 1

---
+++ src/lib/components/ui/navLink/navLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { type Icon as IconType } from 'lucide-svelte';
+	import { page } from '$app/stores';
+
+	interface Props {
+		Icon: typeof IconType;
+		href: string;
+		label: string;
+	}
+
+	let { Icon, href, label }: Props = $props();
+	const active = $page.url.pathname.startsWith(href);
+</script>
+
+<a {href} class={`flex gap-4 ${active ? 'bg-[#E9E9E9]' : ''}`}><Icon /> {label}</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-	import "../app.css";
+	import '../app.css';
+	import { Home, List, CirclePlus, SlidersHorizontal } from 'lucide-svelte';
+	import NavLink from '$lib/components/ui/navLink/navLink.svelte';
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen">
-	<nav class="bg-gray-800 p-4">
-		<div class="container mx-auto flex items-center justify-between">
-			<div class="flex space-x-4">
-				<a href="/" class="text-white hover:text-gray-300">Home</a>
-				<a href="/tasks" class="text-white hover:text-gray-300">Zadania</a>
-				<a href="/tasks/add" class="text-white hover:text-gray-300">Dodaj zadanie</a>
-				<a href="/settings" class="text-white hover:text-gray-300">Ustawienia</a>
+<div class="flex min-h-screen">
+	<nav class="min-h-screen max-w-max rounded-sm border border-black">
+		<div class="border-b border-black p-6">
+			<h1 class="font-italiana text-7xl font-normal">TDM</h1>
+		</div>
+		<div class="flex items-center justify-between p-8">
+			<div class="flex flex-col gap-6">
+				<NavLink href="/" Icon={Home} label="Home" />
+				<a href="/" class="flex gap-4"><Home /> Home</a>
+				<a href="/tasks" class="flex gap-4"><List />Zadania</a>
+				<a href="/tasks/add" class="flex gap-4"><CirclePlus /> zadanie</a>
+				<a href="/settings" class="flex gap-4"><SlidersHorizontal /> Ustawienia</a>
 			</div>
 		</div>
 	</nav>
